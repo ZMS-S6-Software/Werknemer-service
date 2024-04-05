3cc8d176ddbae416b7d18db4defa5705c98d53ee
Mattijs Pronk <456410@student.fontys.nl>
2024-04-05 11:50:01 +0200

    db connectie toegevoegd

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-// iets importeren
-import * as fs from 'fs';
-import {fileReader} from 'fs';
+//iets importeren
+import express from 'express';
+import sql from 'mssql';
+import dotenv from 'dotenv';
 
-//nodejs documentatie
-// https://nodejs.org/api/fs.html
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Configuratie voor databaseverbinding
+const config = {
+    user: 'sa',
+    password: 'admin',
+    server: 'DESKTOP-T5DHN9T',
+    database: 'master',
+    options: {
+        encrypt: false
+    }
+};
+
+// Connectie maken met de database
+sql.connect(config)
+  .then(() => {
+    console.log('Database verbonden');
+    
+    // Server starten nadat de databaseverbinding tot stand is gebracht
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    // Controller inlezen nadat de databaseverbinding tot stand is gebracht
+   
+  })
+  .catch(err => {
+    console.error('Fout bij het verbinden met de database:', err);
+  });
